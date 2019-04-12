f3ac6f4ab926d4c42488fe006f114118b311c200
schredder07 <49495462+schredder07@users.noreply.github.com>
2019-04-12 14:56:06 +0200

    Add files via upload

--- Node.h
+++ Node.h
@@ -1,13 +1,33 @@
 #ifndef _NODE_H
 #define _NODE_H
+template <typename T>
 class Node
 {
 public:
-	int key;
+	T key;
 	Node * next, * prev;
 public:
 	Node();
-	Node(int key, Node * next = 0, Node * prev = 0);
+	Node(T key, Node * next = 0, Node * prev = 0);
 	~Node();
 };
+
+template <typename T>
+Node<T>::Node()
+{
+	next = 0;
+	prev = 0;
+}
+template <typename T>
+Node<T>::Node(T key, Node * next, Node * prev)
+{
+	this->key = key;
+	this->next = next;
+	this->prev = prev;
+}
+template <typename T>
+Node<T>::~Node()
+{
+}
+
 #endif
